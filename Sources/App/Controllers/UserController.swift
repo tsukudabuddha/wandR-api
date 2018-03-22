d0149cc832266fc0df6cb47b01ba1338169cdd21
tsukudabuddha <andrewtsukuda@gmail.com>
2018-03-22 16:35:35 -0700

    Added user controller

---
+++ Sources/App/Controllers/UserController.swift
@@ -0,0 +1,9 @@
+import Vapor
+
+final class UserController {
+    
+    func list(_ req: Request) throws -> ResponseRepresentable {
+        let list = try User.all()
+        return try list.makeJSON()
+    }
+}
