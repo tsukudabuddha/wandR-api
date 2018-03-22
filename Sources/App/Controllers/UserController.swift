829d214558224c1321efdc2791144bf2b6b6268a
tsukudabuddha <andrewtsukuda@gmail.com>
2018-03-22 16:57:07 -0700

    Created single user and create functions

--- Sources/App/Controllers/UserController.swift
+++ Sources/App/Controllers/UserController.swift
@@ -7,4 +7,22 @@ final class UserController {
         return try list.makeJSON()
     }
     
+    func singleUser(_ req: Request) throws -> ResponseRepresentable {
+        guard let userId = req.parameters["id"]?.int else {
+            return Response(status: .badRequest)
+        }
+        
+        return try User.find(userId)!.makeJSON()
+    }
+    
+    func create(_ req: Request) throws -> ResponseRepresentable {
+        
+        guard let username = req.data["username"]?.string else {
+            return Response(status: .badRequest)
+        }
+        
+        let user = User(username: username)
+        try user.save()
+        return Response(redirect: "/users")
+    }
 }
