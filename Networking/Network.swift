b1e35dc05537131db6bb695faebf3d15e2b2002d
tsukudabuddha <andrewtsukuda@gmail.com>
2018-03-26 19:10:17 -0700

    Added routes for search and popular photos

---
+++ Networking/Network.swift
@@ -0,0 +1,8 @@
+//
+//  Network.swift
+//  wandR-apiPackageDescription
+//
+//  Created by Andrew Tsukuda on 3/22/18.
+//
+
+import Foundation
--- Sources/App/Routes/Routes.swift
+++ Sources/App/Routes/Routes.swift
@@ -11,11 +11,20 @@ extension Droplet {
         post("users", handler: userController.create)
         delete("users", handler: userController.remove)
         
-        get("hello") { req in
-            var json = JSON()
-            try json.set("hello", "world")
-            return json
+        get("popular") { req in
+            let photosResponse = try self.client.get("https://api.unsplash.com/photos", ["Authorization" : "Client-ID 0c0435fc2b2eaa7968f2b6f91c5cfb706363ac15b5acf50449a533339fdf31c2"
+                ])
+            let something = photosResponse.json!["urls"]!
+            return something["regular"]!
         }
+        
+        get("search") { req in
+            let photoResponse = try self.client.get("https://api.unsplash.com/search/photos?per_page=50&query=\(req.parameters["query"])", ["Authorization" : "Client-ID 0c0435fc2b2eaa7968f2b6f91c5cfb706363ac15b5acf50449a533339fdf31c2"
+                ])
+            let urls = photoResponse.json?["results"]?["urls"]?["regular"]
+            return urls!
+            
+            }
 
         get("plaintext") { req in
             return "Hello, world!"
